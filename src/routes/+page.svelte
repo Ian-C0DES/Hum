77e4874eb686f613f467427d76685b8948d9d46a
iando <iandowlatkhah@gmail.com>
2023-02-16 13:39:47 -0500

    edits and branching

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,9 +25,26 @@
 <svelte:component this={Waves} />
 </div> -->
 
+<div class="loginContainer">
+    <h1>Login</h1>
+    <form action="" method="post">
+
+        <input type="text" placeholder="Enter Username" name="uname" required>
+    
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <div class="buttons">
+        <button type="submit">Login</button>
+        <a href="/about"><button>Register</button></a>
+        </div>
+
+    </form>
+</div>
+
 <style lang="scss">
     *{
-        // border: solid 1px red;
+        outline: solid 1px red;
+        font-family: "JetBrains Mono";
     }
     .logoContainer{
         display: inline-block;
@@ -41,8 +58,17 @@
     }
     .bannerContainer{
         display: flex;
+        background: radial-gradient(133.53% 172.98% at 0% 0%, #151515 31.85%, #3E2BD2 100%);
+        width: fit-content;
+        transform: translateY(100px);
+        // clip-path: inset(30px);
+        // height: 200px;
+        margin-left: 5em;
+        background-repeat: no-repeat;
+        background-attachment: left bottom;
+        background-position: center;
         p{
-            margin-left: 5rem;
+            margin-left: 1rem;
             font-size: 64px;
             color: var(--textcolor);
             font-family: "JetBrains Mono";
@@ -50,5 +76,41 @@
         
     }
 
+    .loginContainer{
+    position: absolute;
+    right: 10vw;
+    top: 30vh;
+    h1{
+        font-family: "Iceland", cursive;
+        text-align: center;
+        font-size: 64px;
+        color: var(--textcolor);
+    }
+        form{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            // background-color: #151515;
+            width: fit-content;
+            input{
+                margin-top: 20px;
+                background-color: var(--textcolor);
+                color: var(--accent1);
+                width: 500px;
+            }
+            .buttons{
+                display: flex;
+                width: 20rem;
+                justify-content: space-between;
+                margin-top: 20px;
+                button{
+                    
+                }
+                button:hover {
+                     background: radial-gradient(133.53% 172.98% at 0% 0%, #151515 31.85%, #3E2BD2 100%);
+                    }
+            }
+        }
+    }
 
 </style>
