6c0b066d47632f8aca21045507d0aded517df04d
Ian-C0DES <iandowlatkhah@gmail.com>
2023-04-09 22:31:14 -0400

    _

--- src/lib/components/Badge.svelte
+++ src/lib/components/Badge.svelte
@@ -46,36 +46,36 @@ let imageUrl;
 import { page } from '$app/stores';
 
 let filename="";
-const getImageUrl = (name,tier) =>{
-    let src="";
-    switch (tier) {
-        case 1:
-            filename += "I_";
-            break;
-        case 2:
-            filename += "II_";
-            break;
-        case 3:
-            filename += "III_";
-            break;
-        case 4:
-            filename += "IIII_";
-            break;
-        case 5:
-            filename += "IIIII_";
-            break;
-        default:
-            break;
-    }
-    for (let index = 0; index < ($page.route.id.match(/\//g) || []).length; index++) {
-        src += "../"
-    }
-    filename+=name;
-    src += "src/lib/assets/images/badges/"+filename+".png";
-    return src;
-};
+// const getImageUrl = (name,tier) =>{
+//     let src="";
+//     switch (tier) {
+//         case 1:
+//             filename += "I_";
+//             break;
+//         case 2:
+//             filename += "II_";
+//             break;
+//         case 3:
+//             filename += "III_";
+//             break;
+//         case 4:
+//             filename += "IIII_";
+//             break;
+//         case 5:
+//             filename += "IIIII_";
+//             break;
+//         default:
+//             break;
+//     }
+//     for (let index = 0; index < ($page.route.id.match(/\//g) || []).length; index++) {
+//         src += "../"
+//     }
+//     filename+=name;
+//     src += "src/lib/assets/images/badges/"+filename+".png";
+//     return src;
+// };
 
-let badgeImage = getImageUrl(name=name,tier=tier);
+// let badgeImage = getImageUrl(name=name,tier=tier);
 
 </script>
 
