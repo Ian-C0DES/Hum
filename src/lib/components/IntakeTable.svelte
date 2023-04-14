3d0bbf19bcd57404f2e7266c472568375a975455
Nic Bechtel <nbechtel@ggc.edu>
2023-04-14 00:23:15 -0500

    Creaated nutrition page and intake table component

---
+++ src/lib/components/IntakeTable.svelte
@@ -0,0 +1,218 @@
+<script>
+    // import { fade, fly } from 'svelte/transition';
+    // import { foodItems } from '$lib/utils.js';
+    import IntakeRow from '$lib/components/IntakeRow.svelte';
+    export let mealtype;
+    let itemNum = 2;
+
+    const addCard = () => {
+		if(itemNum ==0){
+            itemNum+=2;
+
+        } 
+        else if (itemNum < 20) {
+			itemNum++;
+			console.log(itemNum);
+		}
+	};
+	const removeCard = () => {
+		(itemNum > 0 ? itemNum-- : null);
+	};
+</script>
+
+<div class="btnContainer">
+    <button on:click={addCard} id="addItemBTN"><i class="fa-solid fa-plus" /></button>
+    <button on:click={removeCard} id="removeItemBTN"
+        ><i class="fa-solid fa-minus" /></button
+    >
+</div>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th> <span class="rgtext">{mealtype}</span> </th>
+          <th>Item</th>
+          <th>Quantity</th>
+          <th>Calories</th>
+          <th>Protein</th>
+          <th>Carbs</th>
+          <th>Fats</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+
+  <div class="tbl-content">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <tbody>
+        {#each Array(itemNum) as _, i}
+        <!-- <li>{i + 1}</li> -->
+        <IntakeRow itemNum={i++} />
+        {/each}
+      </tbody>
+    </table>
+  </div>
+
+  <div class="tbl-footer">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th>
+            <button class="clearbtn"> Clear </button>
+          </th>
+          <th>
+            <button class="savebtn"> Save </button>
+          </th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+
+<style lang="scss">
+    table{
+  width:100%;
+  table-layout: fixed;
+  font-family: var(--font);
+}
+.tbl-header{
+    background: radial-gradient(150.81% 167.43% at 0% 0%, rgba(0,0,0,.2) 31.85%, rgba(0,0,0,.05) 100%);
+  //   background-blend-mode:color-burn(var(--accent1));
+}
+.tbl-footer{
+    background: radial-gradient(150.81% 167.43% at 0% 0%, rgba(255,255,255,.2) 31.85%, rgba(255,255,255,.05) 100%);
+    // background: radial-gradient(150.81% 167.43% at 0% 0%, rgba(255, 255, 255, 0.2) rgba(255, 255, 255, 0.05) 100%);
+  //   background-blend-mode:color-burn(var(--accent1));
+  .savebtn{
+      all: unset;
+      position: relative;
+      left: 60%;
+      padding: 5%;
+      border-radius: 18px;
+      cursor: pointer;
+      color: gray;
+      &:hover{
+            background-color: rgba(0,0,0,.2);
+            color: var(--textcolor);
+        }
+
+    }
+    .clearbtn{
+        all: unset;
+        position: relative;
+        left: 130%;
+        padding: 5%;
+        border-radius: 18px;
+        cursor: pointer;
+        color: gray;
+        &:hover{
+            background-color: rgba(0,0,0,.2);
+            color: var(--textcolor);
+        }
+  }
+}
+.tbl-content{
+    // background: radial-gradient(150.81% 167.43% at 0% 0%, rgba(0,0,0,.2) 31.85%, rgba(0,0,0,.05) 100%);
+    height:fit-content;
+    padding-bottom: 5vh;
+    margin-top: 0px;
+    border: 1px solid rgba(255,255,255,0.1);
+    overflow-x:hidden;
+}
+th{
+    padding: 20px 15px;
+    text-align: left;
+    font-weight: 500;
+    font-size: .8rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: 900;
+}
+td{
+  padding: 15px;
+  text-align: left;
+  vertical-align:middle;
+  font-weight: 300;
+  font-size: 12px;
+  color: #fff;
+  border-bottom: solid 1px rgba(255,255,255,0.1);
+}
+.exercise {
+    input,
+		select {
+			-webkit-appearance: none;
+			appearance: none;
+			margin: 0px;
+			all: unset;
+			font-family: var(--font);
+			color: var(--textcolor);
+            font-weight: 900;
+			// background-color: rgba($color: #000000, $alpha: .30);
+			// background-color: transparent;
+		}
+				select option {
+					background-color: var(--accent2);
+                    font-weight: 900;
+                    // background-color: radial-gradient(150.81% 167.43% at 0% 0%, rgba(0,0,0,.2) 31.85%, rgba(0,0,0,.05) 100%);
+				}
+				select optgroup {
+					background-color: var(--accent1);
+                    // background-image: radial-gradient(150.81% 167.43% at 0% 0%, rgba(0,0,0,.2) 31.85%, rgba(0,0,0,.05) 100%);
+				}
+			}
+
+    .btnContainer{
+        // outline: red 1px solid;
+        position: absolute;
+        min-width: 90%;
+        text-align: end;
+        font-weight: 900;
+        font-size: 2rem;
+        #addItemBTN{
+            all: unset;
+            // border-radius: 18px;
+            cursor: pointer;
+            // padding: .6% .6% .6% .6%;
+            // margin: 0px;
+            color: grey;
+            // background-color: rgba(255, 255, 255, 0.05);
+            &:hover{
+                background: radial-gradient(150.81% 167.43% at 0% 0%, var(--accent1) 31.85%, var(--accent2) 100%);
+                -webkit-text-fill-color: transparent;
+                -webkit-background-clip: text;
+                background-clip: text;
+                color: transparent;
+            }
+        }
+        #removeItemBTN{
+            all: unset;
+            cursor: pointer;
+            padding: .6% .6% .6% .6%;
+            // margin: 0px;
+            color: grey;
+            // background-color: rgba(255, 255, 255, 0.05);
+            &:hover{
+                background: radial-gradient(150.81% 167.43% at 0% 0%, var(--accent1) 31.85%, var(--accent2) 100%);
+                -webkit-text-fill-color: transparent;
+                -webkit-background-clip: text;
+                background-clip: text;
+                color: transparent;
+            }
+        }
+    }
+/* for custom scrollbar for webkit browser*/
+
+::-webkit-scrollbar {
+    width: 6px;
+} 
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+} 
+::-webkit-scrollbar-thumb {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+}
+</style>
---
+++ src/routes/nutrition/page.svelte
@@ -0,0 +1,277 @@
+<script>
+ import { onMount } from "svelte";
+ import IntakeTable from '$lib/components/IntakeTable.svelte';
+ let dateNow = new Date();
+
+ $: time = dateNow.toLocaleTimeString();
+ $: date = dateNow.toDateString();
+ $: mealType = getMealType(new Date().getHours());
+
+
+
+
+
+
+
+
+
+
+ const getMealType = (hours) => {
+		console.log(hours);	
+		let mealType;
+		
+		if(hours >= 4 && hours <= 11){
+		mealType = 'Breakfast'
+	}
+	else if  (hours >= 12 && hours <= 15){
+		mealType = 'Lunch'
+	} else if (hours >= 17 && hours <= 22){
+		
+		mealType = 'Dinner'
+	} else{
+		mealType = 'Snack'	
+	}
+		return mealType;
+	};
+
+ onMount(() => {
+   const timerId = setInterval(() => {
+	dateNow = new Date();
+   }, 1000);
+   return () => {
+     clearInterval(timerId);
+   };
+ });
+
+</script>
+
+<body>
+    <div id="content">
+        
+		<div class="bannerContainer">
+			<div class="banner">
+
+				<div class="datetimeContainer"> 
+					<span class="date">{date}</span>
+					<span class="time rgtext">{time}</span>
+				</div>
+
+				<div class="mealtime underline-gradient">
+					<span>
+						<i class="fa-solid fa-apple-whole"></i>
+					</span>
+					{mealType}
+				</div>
+
+
+				<div class="metricsContainer">
+
+					<div class="calories rgtext">
+						100 Calories
+					</div>
+				
+					<div class="protein">
+						<span>
+							<i class="fa-solid fa-drumstick-bite"></i>
+						</span>
+						<span class="macro">	
+						PROTEIN 
+						</span>	
+						<span class="arrows">
+							→
+						</span>
+						<span class="number">	
+							13
+						</span>
+						g
+					</div>
+
+					<div class="carbs">
+						<span>
+							<i class="fa-solid fa-wheat-awn"></i>
+						</span>
+						<span class="macro">
+							CARBS 
+						</span>
+						<span class="arrows">
+							→
+						</span>
+						<span class="number">
+							25
+						</span>
+						 g
+					</div>
+					
+					<div class="fats">
+						<span>
+							<i class="fa-solid fa-bacon"></i>
+						</span>
+						<span class="macro">
+							FATS 
+						</span>
+						<span class="arrows">
+							→
+						</span>
+						<span class="number">
+
+							10
+						</span>
+						g
+					</div>
+
+				</div>
+				<!-- //TODO: meal type (breakfast brunch lunch dinner, 3 hours since last ate? snack:stomach )) -->
+			</div>
+		</div>
+
+
+
+<div>
+<IntakeTable mealtype={mealType}/>
+</div>
+
+
+<div class="controls">
+<!-- //TODO: buttons here (select day, add table row, change to liquids/pills.) -->
+</div>
+
+    </div>
+</body>
+
+
+<style lang="scss">
+* {
+		// outline: 1px red solid;
+		// color: yellow;
+	}
+	body {
+		min-height: 100vh;
+		overflow-x: hidden;
+		background: radial-gradient(
+			400.81% 400.43% at -275% -220%,
+			var(--textcolor) 40.22%,
+			var(--dark) 100%
+		);
+	}
+	#content {
+		margin-left: 8vw;
+		max-width: 90vw;
+    }
+	.bannerContainer{
+		// background-color: aqua;
+		display: flex;
+		justify-content: center;
+		padding: 5%;
+		
+		.banner{
+			padding: 3%;
+			// background: radial-gradient(150.81% 167.43% at 0% 0%, var(--accent2) 31.85%, var(--textcolor) 100%);
+			background: radial-gradient(150.81% 167.43% at 0% 0%, rgba(0,0,0,.2) 31.85%, rgba(0,0,0,.05) 100%);
+			width: 60%;
+			display: flex;
+			border-radius: 18px;
+		}
+		.datetimeContainer{
+			display: flex;
+			flex-direction: column;
+			.date{
+				font-family: var(--font);
+				font-weight: 900;
+				color: var(--textcolor);
+				font-size: 2rem;
+			}
+			.time{
+				font-weight: 600;
+				font-family: var(--font);
+				font-size: 1.4rem;
+
+			}
+		}
+		.mealtime{
+			font-family: var(--font);
+			position: relative;
+			right: 33%;
+			top: 80%;
+			height: fit-content;
+			min-width: fit-content;
+			font-size: 2rem;
+			font-weight: 900;
+			color: #ffffff;
+			span{
+				i{
+					background: radial-gradient(150.81% 167.43% at 0% 0%, rgb(248, 137, 137) 31.85%, rgb(255, 0, 0) 100%);
+					-webkit-text-fill-color: transparent;
+					-webkit-background-clip: text;
+					background-clip: text;
+					color: transparent;
+					position: relative;
+					bottom: .2rem;
+				}
+			}
+		}
+		.metricsContainer{
+			// margin-top: 7%;
+			position: relative;
+			right: 10%;
+			// bottom: 20%;
+			min-width: fit-content;
+			font-size: 2rem;
+			font-weight: 900;
+			color: var(--textcolor);
+			
+			.number, .macro{
+				color: #ffffff;	
+				}
+			.arrows{
+				background: radial-gradient(150.81% 167.43% at 0% 0%, var(--accent1) 31.85%, var(--accent2) 100%);
+				-webkit-text-fill-color: transparent;
+				-webkit-background-clip: text;
+				background-clip: text;
+				color: transparent;
+				font-size: 3rem;
+			}
+			.calories{
+				text-align: center;
+				font-family: var(--font);
+			}
+			.protein{
+				font-family: var(--font);
+				span{
+					font-family: var(--font);
+					i{
+						background: linear-gradient(rgb(102, 68, 41), rgb(193, 129, 78) 50%, rgb(193, 129, 78) 80%);
+						-webkit-text-fill-color: transparent;
+						-webkit-background-clip: text;
+						background-clip: text;
+						color: transparent;
+					}
+				}
+
+			}
+			.carbs{
+				font-family: var(--font);
+				span{
+					i{
+				background: linear-gradient(rgb(255, 234, 130), rgb(255, 212, 120) 50%, #cba716 80%);
+				-webkit-text-fill-color: transparent;
+				-webkit-background-clip: text;
+				background-clip: text;
+				color: transparent;
+					}
+				}
+			}
+			.fats{
+				font-family: var(--font);
+				span{
+					i{
+					background: linear-gradient(rgb(255, 167, 167), rgb(243, 128, 128) 50%, #ffeaea 80%);
+					-webkit-text-fill-color: transparent;
+					-webkit-background-clip: text;
+					background-clip: text;
+					color: transparent;
+					}
+				}
+			}
+		}
+	}
+</style>
