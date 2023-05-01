b0a3e2c52772e55c8a0992b7700528e9892ec856
Joseph Kolarik <jkolarik@ggc.edu>
2023-05-01 19:15:39 -0400

    Adds Pull-up workout

--- src/lib/components/WorkoutCard.svelte
+++ src/lib/components/WorkoutCard.svelte
@@ -36,7 +36,7 @@
 		},
 		{
 			optGroup: 'Shoulder & Back',
-			opts: ['Upright Row', 'Shoulder Press', 'Lateral Pulldown']
+			opts: ['Upright Row', 'Shoulder Press', 'Lateral Pulldown', 'Pull-up']
 		}
 	];
 
